9ba27cdb571dc94afa081ef3d95a508211114351
taiwania <taiwaniajusto@gmail.com>
2023-07-23 02:34:34 +0800

    Set login function

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const passport = require('passport')
 const app = express()
 
 // Set the port
@@ -18,6 +21,10 @@ app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(
+  session({ secret: 'keyboard cat', resave: false, saveUninitialized: true })
+)
+usePassport(app)
 
 // Routes
 app.get('/', (req, res) => {
@@ -44,9 +51,13 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post(
+  '/users/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+  })
+)
 
 app.get('/users/register', (req, res) => {
   res.render('register')
