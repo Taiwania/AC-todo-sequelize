57abe63dc333911a30d2a5976c056f5cc9ef5512
taiwania <taiwaniajusto@gmail.com>
2023-07-23 00:28:14 +0800

    Build the login/register pages and routes

--- app.js
+++ app.js
@@ -16,7 +16,27 @@ app.use(methodOverride('_method'))
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 // Listener
