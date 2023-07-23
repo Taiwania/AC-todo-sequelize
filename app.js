c40d1e3459414594a79c93bc2612a174cf2dbb92
taiwania <taiwaniajusto@gmail.com>
2023-07-23 18:56:24 +0800

    Set routers

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express()
 
 // Set the port
 const PORT = 3000
+const router = require('./routes')
 
 // Import database and models
 const db = require('./models')
@@ -27,72 +28,7 @@ app.use(
 usePassport(app)
 
 // Routes
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true,
-  })
-    .then((todos) => {
-      return res.render('index', { todos: todos })
-    })
-    .catch((error) => {
-      return res.status(422).json(error)
-    })
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then((todo) => res.render('detail', { todo: todo.toJSON() }))
-    .catch((error) => console.log(error))
-})
-
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-app.post(
-  '/users/login',
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/users/login',
-  })
-)
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } }).then((user) => {
-    if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword,
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then((salt) => bcrypt.hash(password, salt))
-      .then((hash) =>
-        User.create({
-          name,
-          email,
-          password: hash,
-        })
-      )
-      .then(() => res.redirect('/'))
-      .catch((err) => console.log(err))
-  })
-})
-
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+app.use(router)
 
 // Listener
 app.listen(PORT, () => {
---
+++ routes/modules/home.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+const db = require('../../models')
+const User = db.User
+const Todo = db.Todo
+
+router.get('/', (req, res) => {
+  return Todo.findAll({
+    raw: true,
+    nest: true,
+  })
+    .then((todos) => {
+      return res.render('index', { todos: todos })
+    })
+    .catch((error) => {
+      return res.status(422).json(error)
+    })
+})
+
+router.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then((todo) => res.render('detail', { todo: todo.toJSON() }))
+    .catch((error) => console.log(error))
+})
+
+module.exports = router
